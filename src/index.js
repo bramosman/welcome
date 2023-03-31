1b93408ea6fac41e66fa58fcc7fac26b4758dbab
My namebram@theosmans.com <bram@upstager.co>
2023-03-30 20:08:15 -0400

    config

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+import {getAuth} from "firebase/auth";
+
+
+
+const firebaseApp = initializeApp ({  
+    apiKey: "AIzaSyBTAPVYUBOU-paHL5Z5T7pRRov9qBh2RVs",
+    authDomain: "bram-377904.firebaseapp.com",
+    projectId: "bram-377904",
+    storageBucket: "bram-377904.appspot.com",
+    messagingSenderId: "221784747699",
+    appId: "1:221784747699:web:dde37f48496bd481c28ac6",
+    measurementId: "G-19DRWPJWWJ"
+});
+
+
+
+
+
+
